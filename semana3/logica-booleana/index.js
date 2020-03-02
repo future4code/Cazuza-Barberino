d751553369518469ad35e4b624e98ad118d2e803
BroodMaiden <41172576+BroodMaiden@users.noreply.github.com>
2020-03-02 12:35:51 -0300

    logica booleana

---
+++ semana3/logica-booleana/index.js
@@ -0,0 +1,68 @@
+//1)
+// a.  false
+// b.  false
+// c.  true
+// d.  false
+// e.  boolean
+
+//2)
+//a. array é uma variável capaz de guardar varios valores. let array = [a, b, .. , c];
+//b. 0
+//c. array.length
+//d. 
+// I.  undefined
+// II.  null
+// III.  11
+// IV.  3  e  4
+// V.  19  e  9
+// VI.  3
+// VII.  1
+
+//3.1)
+
+// a)
+// let f = 77;
+// let k = (f - 32) * 5 / 9 + 273.15;
+// console.log(k + "K");
+
+// b)
+// let c = 80;
+// let f = c * 9/5  + 32;
+// console.log(f + "°F");
+
+// c)
+// let c = 30;
+// let f = c * 9/5  + 32;
+// let k = (f - 32) * 5 / 9 + 273.15;
+// console.log(k + "K | " + f + "°F");
+
+// d)
+// let c = prompt("Insira temperatura em graus celsius");
+// let f = c * 9/5  + 32;
+// let k = (f - 32) * 5 / 9 + 273.15;
+// console.log(k + "K | " + f + "°F");
+
+// 3.2)
+
+// let r;
+// r = prompt("1. Qual o seu nome?");
+// console.log("Resposta: " + r);
+// r = prompt("2. Onde moras?");
+// console.log("Resposta: " + r);
+// r = prompt("3. Qual a sua idade?");
+// console.log("Resposta: " + r);
+// r = prompt("4. Tem irmão?");
+// console.log("Resposta: " + r);
+// r = prompt("5. Qual o seu lazer?");
+// console.log("Resposta: " + r);
+
+// 3.3)
+
+// let salario = prompt("Salário Mínimo");
+// let qw = prompt("Quilowatts");
+
+// // console.log(qw * .05);
+
+// let desc = prompt("Desconto");
+
+// console.log(qw * .05 * (1 - desc));
