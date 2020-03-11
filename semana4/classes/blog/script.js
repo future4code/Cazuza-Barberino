a6b849122ec2422c5bb8f4a7029fd9a22efca4cd
BroodMaiden <41172576+BroodMaiden@users.noreply.github.com>
2020-03-11 14:50:27 -0300

    5

--- semana4/classes/blog/script.js
+++ semana4/classes/blog/script.js
@@ -1,34 +1,51 @@
-
 let postForm = document.getElementById("post-form");
 let postArea = document.getElementById("post-area");
 
 let postList = [];
 
-class Post{
-    constructor(title, author, content){
-        this.title = title;
-        this.author = author;
-        this.content = content;
-    }
+class Post {
+  constructor(title, author, content) {
+    this.title = title;
+    this.author = author;
+    this.content = content;
+  }
 }
 
+const NewPost = () => {
+  let title = document.getElementById("title");
+  let author = document.getElementById("author");
+  let content = document.getElementById("content");
 
-const NewPost = () =>{
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let content = document.getElementById("content").value;
+  let post = new Post(title.value, author.value, content.value);
 
-    postList.push(new Post(title, author, content));
+  CreatePost(post);
 
-    console.log(postList);
+  postList.push(post);
+  console.log(postList);
+
+  title.value = author.value = content.value = "";
+};
+
+const CreatePost = post =>{
+    postArea.innerHTML +=
+    "<div class='post'>" +
+    "<h2>" +
+    post.title +
+    "</h2>" +
+    "<h3>" +
+    post.author +
+    "</h3>" +
+    "<p>" +
+    post.content +
+    "</p>";
 }
 
-const GoToPosts = () =>{
-    postForm.style.display = "none";
-    postArea.style.display = "flex";
-}
+const GoToPosts = () => {
+  postForm.style.display = "none";
+  postArea.style.display = "flex";
+};
 
-const GoToCreatePosts = () =>{
-    postForm.style.display = "flex";
-    postArea.style.display = "none";
-}
+const GoToCreatePosts = () => {
+  postForm.style.display = "flex";
+  postArea.style.display = "none";
+};
