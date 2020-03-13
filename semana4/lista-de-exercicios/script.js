5e16337815ca65c70272b968b2d40616cc5b9e59
BroodMaiden <41172576+BroodMaiden@users.noreply.github.com>
2020-03-13 13:08:16 -0300

    Funcoes-de-Arrays-5

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -347,3 +347,25 @@ const emails = consultas.map(pessoa => {
         ". Por favor, acuse o recebimento deste e-mail.")
   );
 });
+
+// 5)
+
+const contas = [
+  { cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+  { cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+  { cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+  { cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+  { cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+  { cliente: "Soter", saldoTotal: 1200, compras: [] }
+];
+
+const atualizarSaldos = arr =>{
+    arr.forEach(conta =>{
+        conta.compras.forEach(compra=>{
+            conta.saldoTotal -= compra;
+        })
+    })
+}
+
+atualizarSaldos(contas);
+
